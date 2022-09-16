ba1d92e6b7d3f10e6114bd6a9e1e1865196bada8
Coleman <charleebrown22@gmail.com>
2022-09-16 15:16:16 -0500

    Added Chakra-UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import ClassPick from './components/ClassPick/ClassPick.tsx'
+import {ChakraProvider} from  'chakra-ui'
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <header className="App-header">
        <ClassPick></ClassPick>
       </header>
+
     </div>
+    </ChakraProvider>
   );
 }
 
